3b8225cb04e4875a27e71d9ce21b270abd711b1b
quintois <arnaud.quintois@etu.u-pec.fr>
2020-05-06 08:56:23 +0000

    creation_questions

---
+++ creation_questions.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title>Créations questions</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<?php
+		/*if(!isset($_COOKIE['user'])) {
+        	if(isset($_POST['user'])) {
+        	    setcookie('user', $_POST['user']);
+        	    $user = json_decode($_POST['user']); //////////////// A METTRE DANS LA PAGE APRES CETTE PAGE 
+        	}
+        	else {
+        	    header('Location: https://dwarves.iut-fbleau.fr/~leblet/WIM/PROJET/login.php');
+        	}
+    	} else {
+        	$user = json_decode($_COOKIE['user']);
+    	} */
+    ?>
+    <form method="POST">
+    	<input type="text" name="theme" placeholder="nom du thème:" required>
+    	<br/><br/>
+
+    	<input type="text" name="question1" placeholder="Question 1:" required>
+    	<br/>
+    	<input type="text" name="reponse_1_a" placeholder="Reponse A:" required>
+    	<input type="radio" name="reponse1">
+    	<br/>
+    	<input type="text" name="reponse_1_b" placeholder="Reponse B:" required>
+    	<input type="radio" name="reponse1">
+    	<input type="text" name="reponse_1_c" placeholder="Reponse C:" required>
+    	<input type="radio" name="reponse1">
+    	<br/>
+    	<input type="text" name="reponse_1_d" placeholder="Reponse D:" required>
+    	<input type="radio" name="reponse1">
+    	
+
+    	<input type="text" name="question2" placeholder="Question 2:" required>
+    	<br/>
+    	<input type="text" name="reponse_2_a" placeholder="Reponse A:" required>
+    	<input type="radio" name="reponse2">
+    	<br/>
+    	<input type="text" name="reponse_2_b" placeholder="Reponse B:" required>
+    	<input type="radio" name="reponse2">
+    	<input type="text" name="reponse_2_c" placeholder="Reponse C:" required>
+    	<input type="radio" name="reponse2">
+    	<br/>
+    	<input type="text" name="reponse_2_d" placeholder="Reponse D:" required>
+    	<input type="radio" name="reponse2">
+
+
+    	<input type="text" name="question3" placeholder="Question 3:" required>
+    	<br/>
+    	<input type="text" name="reponse_3_a" placeholder="Reponse A:" required>
+    	<input type="radio" name="reponse3">
+    	<br/>
+    	<input type="text" name="reponse_3_b" placeholder="Reponse B:" required>
+    	<input type="radio" name="reponse3">
+    	<input type="text" name="reponse_3_c" placeholder="Reponse C:" required>
+    	<input type="radio" name="reponse3">
+    	<br/>
+    	<input type="text" name="reponse_3_d" placeholder="Reponse D:" required>
+    	<input type="radio" name="reponse3">
+
+
+    	<input type="text" name="question4" placeholder="Question 4:" required>
+    	<br/>
+    	<input type="text" name="reponse_4_a" placeholder="Reponse A:" required>
+    	<input type="radio" name="reponse4">
+    	<br/>
+    	<input type="text" name="reponse_4_b" placeholder="Reponse B:" required>
+    	<input type="radio" name="reponse4">
+    	<input type="text" name="reponse_4_c" placeholder="Reponse C:" required>
+    	<input type="radio" name="reponse4">
+    	<br/>
+    	<input type="text" name="reponse_4_d" placeholder="Reponse D:" required>
+    	<input type="radio" name="reponse4">
+
+
+    	<input type="text" name="question5" placeholder="Question 5:" required>
+    	<br/>
+    	<input type="text" name="reponse_5_a" placeholder="Reponse A:" required>
+    	<input type="radio" name="reponse5">
+    	<br/>
+    	<input type="text" name="reponse_5_b" placeholder="Reponse B:" required>
+    	<input type="radio" name="reponse5">
+    	<input type="text" name="reponse_5_c" placeholder="Reponse C:" required>
+    	<input type="radio" name="reponse5">
+    	<br/>
+    	<input type="text" name="reponse_5_d" placeholder="Reponse D:" required>
+    	<input type="radio" name="reponse5">
+
+
+    	<input type="text" name="question6" placeholder="Question 6:" required>
+    	<br/>
+    	<input type="text" name="reponse_6_a" placeholder="Reponse A:" required>
+    	<input type="radio" name="reponse6">
+    	<br/>
+    	<input type="text" name="reponse_6_b" placeholder="Reponse B:" required>
+    	<input type="radio" name="reponse6">
+    	<input type="text" name="reponse_6_c" placeholder="Reponse C:" required>
+    	<input type="radio" name="reponse6">
+    	<br/>
+    	<input type="text" name="reponse_6_d" placeholder="Reponse D:" required>
+    	<input type="radio" name="reponse6">
+
+
+    	<input type="text" name="question7" placeholder="Question 7:" required>
+    	<br/>
+    	<input type="text" name="reponse_7_a" placeholder="Reponse A:" required>
+    	<input type="radio" name="reponse7">
+    	<br/>
+    	<input type="text" name="reponse_7_b" placeholder="Reponse B:" required>
+    	<input type="radio" name="reponse7">
+    	<input type="text" name="reponse_7_c" placeholder="Reponse C:" required>
+    	<input type="radio" name="reponse7">
+    	<br/>
+    	<input type="text" name="reponse_7_d" placeholder="Reponse D:" required>
+    	<input type="radio" name="reponse7">
+
+
+    	<input type="text" name="question8" placeholder="Question 8:" required>
+    	<br/>
+    	<input type="text" name="reponse_8_a" placeholder="Reponse A:" required>
+    	<input type="radio" name="reponse8">
+    	<br/>
+    	<input type="text" name="reponse_8_b" placeholder="Reponse B:" required>
+    	<input type="radio" name="reponse8">
+    	<input type="text" name="reponse_8_c" placeholder="Reponse C:" required>
+    	<input type="radio" name="reponse8">
+    	<br/>
+    	<input type="text" name="reponse_8_d" placeholder="Reponse D:" required>
+    	<input type="radio" name="reponse8">
+    </form>
+</body>
+</html>
