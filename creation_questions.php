b3fd7401d1c6c40c7c70734c226e8be761b8c0a8
leblet <leblet@localhost>
2020-05-06 17:49:18 +0200

    creation_questions

--- creation_questions.php
+++ creation_questions.php
@@ -25,129 +25,121 @@
     	}
     ?>
     <form method="POST">
-    	<input type="text" name="theme" placeholder="nom du thème:" required>
+    	<input type="text" name="theme" placeholder="Nom du thème" class="theme" required>
     	<br/><br/>
 
     	<div class="question1">
-    		<input type="text" name="question1" placeholder="Question 1:" required>
+    		<input type="text" name="question1" placeholder="Question 1" class="question" required>
     		<br/>
-    		<input type="text" name="reponse_1_a" placeholder="Reponse A:" required>
-    		<input type="radio" name="reponse1">
-    		<input type="text" name="reponse_1_b" placeholder="Reponse B:" required>
-    		<input type="radio" name="reponse1">
+            <input type="text" name="reponse_1_a" placeholder="Reponse A" required>
+            <input type="radio" name="reponse1" >
+    		<input type="text" name="reponse_1_b" placeholder="Reponse B" required>
+    		<input type="radio" name="reponse1" >
     		<br/>
-    		<input type="text" name="reponse_1_c" placeholder="Reponse C:" required>
-    		<input type="radio" name="reponse1">
-    		<input type="text" name="reponse_1_d" placeholder="Reponse D:" required>
-    		<input type="radio" name="reponse1">
-    		<br/>
+            <input type="text" name="reponse_1_c" placeholder="Reponse C" required>
+            <input type="radio" name="reponse1" >            
+    		<input type="text" name="reponse_1_d" placeholder="Reponse D" required>
+    		<input type="radio" name="reponse1" >
     	</div>
 
     	<div class="question2">
-	    	<input type="text" name="question2" placeholder="Question 2:" required>
+	    	<input type="text" name="question2" placeholder="Question 2" class="question" required>
 	    	<br/>
-	    	<input type="text" name="reponse_2_a" placeholder="Reponse A:" required>
-	    	<input type="radio" name="reponse2">
-	    	<input type="text" name="reponse_2_b" placeholder="Reponse B:" required>
-	    	<input type="radio" name="reponse2">
+	    	<input type="text" name="reponse_2_a" placeholder="Reponse A" required>
+	    	<input type="radio" name="reponse2" >
+	    	<input type="text" name="reponse_2_b" placeholder="Reponse B" required>
+	    	<input type="radio" name="reponse2" >
 	    	<br/>
-	    	<input type="text" name="reponse_2_c" placeholder="Reponse C:" required>
-	    	<input type="radio" name="reponse2">
-	    	<input type="text" name="reponse_2_d" placeholder="Reponse D:" required>
-	    	<input type="radio" name="reponse2">
-	    	<br/>
+	    	<input type="text" name="reponse_2_c" placeholder="Reponse C" required>
+	    	<input type="radio" name="reponse2" >
+	    	<input type="text" name="reponse_2_d" placeholder="Reponse D" required>
+	    	<input type="radio" name="reponse2" >
 		</div>
 
     	<div class="question3">
-    		<input type="text" name="question3" placeholder="Question 3:" required>
+    		<input type="text" name="question3" placeholder="Question 3" class="question" required>
     		<br/>
-    		<input type="text" name="reponse_3_a" placeholder="Reponse A:" required>
-    		<input type="radio" name="reponse3">
-    		<input type="text" name="reponse_3_b" placeholder="Reponse B:" required>
-    		<input type="radio" name="reponse3">
+    		<input type="text" name="reponse_3_a" placeholder="Reponse A" required>
+    		<input type="radio" name="reponse3" >
+    		<input type="text" name="reponse_3_b" placeholder="Reponse B" required>
+    		<input type="radio" name="reponse3" >
     		<br/>
-    		<input type="text" name="reponse_3_c" placeholder="Reponse C:" required>
-    		<input type="radio" name="reponse3">
-    		<input type="text" name="reponse_3_d" placeholder="Reponse D:" required>
-    		<input type="radio" name="reponse3">
-    		<br/>
+    		<input type="text" name="reponse_3_c" placeholder="Reponse C" required>
+    		<input type="radio" name="reponse3" >
+    		<input type="text" name="reponse_3_d" placeholder="Reponse D" required>
+    		<input type="radio" name="reponse3" >
     	</div>
 
     	<div class="question4">
-    		<input type="text" name="question4" placeholder="Question 4:" required>
+    		<input type="text" name="question4" placeholder="Question 4" class="question" required>
     		<br/>
-    		<input type="text" name="reponse_4_a" placeholder="Reponse A:" required>
-    		<input type="radio" name="reponse4">
-    		<input type="text" name="reponse_4_b" placeholder="Reponse B:" required>
-    		<input type="radio" name="reponse4">
+    		<input type="text" name="reponse_4_a" placeholder="Reponse A" required>
+    		<input type="radio" name="reponse4" >
+    		<input type="text" name="reponse_4_b" placeholder="Reponse B" required>
+    		<input type="radio" name="reponse4" >
     		<br/>
-    		<input type="text" name="reponse_4_c" placeholder="Reponse C:" required>
-    		<input type="radio" name="reponse4">
-    		<input type="text" name="reponse_4_d" placeholder="Reponse D:" required>
-    		<input type="radio" name="reponse4">
-    		<br/>
+    		<input type="text" name="reponse_4_c" placeholder="Reponse C" required>
+    		<input type="radio" name="reponse4" >
+    		<input type="text" name="reponse_4_d" placeholder="Reponse D" required>
+    		<input type="radio" name="reponse4" >
     	</div>
 
     	<div class="question5">
-    		<input type="text" name="question5" placeholder="Question 5:" required>
+    		<input type="text" name="question5" placeholder="Question 5" class="question" required>
     		<br/>
-    		<input type="text" name="reponse_5_a" placeholder="Reponse A:" required>
-    		<input type="radio" name="reponse5">
-    		<input type="text" name="reponse_5_b" placeholder="Reponse B:" required>
-    		<input type="radio" name="reponse5">
+    		<input type="text" name="reponse_5_a" placeholder="Reponse A" required>
+    		<input type="radio" name="reponse5" >
+    		<input type="text" name="reponse_5_b" placeholder="Reponse B" required>
+    		<input type="radio" name="reponse5" >
     		<br/>
-    		<input type="text" name="reponse_5_c" placeholder="Reponse C:" required>
-    		<input type="radio" name="reponse5">
-    		<input type="text" name="reponse_5_d" placeholder="Reponse D:" required>
-    		<input type="radio" name="reponse5">
-    		<br/>
+    		<input type="text" name="reponse_5_c" placeholder="Reponse C" required>
+    		<input type="radio" name="reponse5" >
+    		<input type="text" name="reponse_5_d" placeholder="Reponse D" required>
+    		<input type="radio" name="reponse5" >
     	</div>
 
     	<div class="question6">
-    		<input type="text" name="question6" placeholder="Question 6:" required>
+    		<input type="text" name="question6" placeholder="Question 6" class="question" required>
     		<br/>
-    		<input type="text" name="reponse_6_a" placeholder="Reponse A:" required>
-    		<input type="radio" name="reponse6">
-    		<input type="text" name="reponse_6_b" placeholder="Reponse B:" required>
-    		<input type="radio" name="reponse6">
+    		<input type="text" name="reponse_6_a" placeholder="Reponse A" required>
+    		<input type="radio" name="reponse6" >
+    		<input type="text" name="reponse_6_b" placeholder="Reponse B" required>
+    		<input type="radio" name="reponse6" >
     		<br/>
-    		<input type="text" name="reponse_6_c" placeholder="Reponse C:" required>
-    		<input type="radio" name="reponse6">
-    		<input type="text" name="reponse_6_d" placeholder="Reponse D:" required>
-    		<input type="radio" name="reponse6">
-    		<br/>
+    		<input type="text" name="reponse_6_c" placeholder="Reponse C" required>
+    		<input type="radio" name="reponse6" >
+    		<input type="text" name="reponse_6_d" placeholder="Reponse D" required>
+    		<input type="radio" name="reponse6" >
     	</div>
 
     	<div class="question7">
-    		<input type="text" name="question7" placeholder="Question 7:" required>
+    		<input type="text" name="question7" placeholder="Question 7" class="question" required>
     		<br/>
-    		<input type="text" name="reponse_7_a" placeholder="Reponse A:" required>
-    		<input type="radio" name="reponse7">
-    		<input type="text" name="reponse_7_b" placeholder="Reponse B:" required>
-    		<input type="radio" name="reponse7">
+    		<input type="text" name="reponse_7_a" placeholder="Reponse A" required>
+    		<input type="radio" name="reponse7" >
+    		<input type="text" name="reponse_7_b" placeholder="Reponse B" required>
+    		<input type="radio" name="reponse7" >
     		<br/>
-    		<input type="text" name="reponse_7_c" placeholder="Reponse C:" required>
-    		<input type="radio" name="reponse7">
-    		<input type="text" name="reponse_7_d" placeholder="Reponse D:" required>
-    		<input type="radio" name="reponse7">
-    		<br/>
+    		<input type="text" name="reponse_7_c" placeholder="Reponse C" required>
+    		<input type="radio" name="reponse7" >
+    		<input type="text" name="reponse_7_d" placeholder="Reponse D" required>
+    		<input type="radio" name="reponse7" >
     	</div>
 
     	<div class="question8">
-    		<input type="text" name="question8" placeholder="Question 8:" required>
+    		<input type="text" name="question8" placeholder="Question 8" class="question" required>
     		<br/>
-    		<input type="text" name="reponse_8_a" placeholder="Reponse A:" required>
-    		<input type="radio" name="reponse8">
-    		<input type="text" name="reponse_8_b" placeholder="Reponse B:" required>
-    		<input type="radio" name="reponse8">
+    		<input type="text" name="reponse_8_a" placeholder="Reponse A" required>
+    		<input type="radio" name="reponse8" >
+    		<input type="text" name="reponse_8_b" placeholder="Reponse B" required>
+    		<input type="radio" name="reponse8" >
     		<br/>
-    		<input type="text" name="reponse_8_c" placeholder="Reponse C:" required>
-    		<input type="radio" name="reponse8">
-    		<input type="text" name="reponse_8_d" placeholder="Reponse D:" required>
-    		<input type="radio" name="reponse8">
+    		<input type="text" name="reponse_8_c" placeholder="Reponse C" required>
+    		<input type="radio" name="reponse8" >
+    		<input type="text" name="reponse_8_d" placeholder="Reponse D" required>
+    		<input type="radio" name="reponse8" >
     		<br/>
     	</div>
-
     	<button type="submit" name="créer">Créer</button> 
     </form>
 
