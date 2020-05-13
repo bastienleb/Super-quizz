5e4d4efff73d7519dd3c0e2123d93552f7f028c7
quintois <arnaud.quintois@etu.u-pec.fr>
2020-05-13 10:11:35 +0000

    creation_salle

--- creation_salle.php
+++ creation_salle.php
@@ -6,6 +6,18 @@
 </head>
 <body>
 	<?php
+		/*if(!isset($_COOKIE['user'])) {
+        	if(isset($_POST['user'])) {
+        	    setcookie('user', $_POST['user']);
+        	    $user = json_decode($_POST['user']); //////////////// A METTRE DANS LA PAGE APRES CETTE PAGE 
+        	}
+        	else {
+        	    header('Location: https://dwarves.iut-fbleau.fr/~leblet/WIM/PROJET/login.php');
+        	}
+    	} else {
+        	$user = json_decode($_COOKIE['user']);
+    	} */
+    	
 		$link=mysqli_connect("localhost","quintois","quintois","quintois");
 		if(!$link){
 			die("<p>connexion impossible</p>");
@@ -17,6 +29,25 @@
 		<input type="text" name="nombre_joueurs" placeholder="nombre de joueurs">
 		<br/>
 		<input type="text" name="nombre_questions" placeholder="nombre de questions">
+		<select name="user_name" class="selecteur">
+	    	<option value="selection" hidden="">Selectionnez</option>
+		
+			<?php
+ 	 			$resultat = mysqli_query($link,"SELECT nom_theme FROM Themes ");
+     			if($resultat) {
+         	?>
+         	<?php
+         		while($nom=mysqli_fetch_object($resultat)) {
+            		echo "<option>";
+            		echo $nom->nom_theme;
+            		echo "</option>";
+        		}
+        		echo "</select>";
+    			} else {
+        			die("<p>erreur dans la requete<p>");
+    			}
+ 			?>
+    	</select>
 	</form>		
 </body>
 </html>
