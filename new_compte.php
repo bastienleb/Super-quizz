4569e74c253f1125609f14cf5aa932346e657136
leblet <leblet@localhost>
2020-05-16 18:58:07 +0200

    new_compte

--- new_compte.php
+++ new_compte.php
@@ -27,7 +27,8 @@
 		<b><label>Confirmer le mot de passe : </label></b><br/>
 		<input type="password" name="confirmation" required>
 		<br/><br/>
-		<button name="nouveau_compte"><b>Envoyer</b></button>
+		<button name="nouveau_compte"><b>Envoyer</b></button><br/>
+		<a href="login.php">connexion</a>
 	</form>
 	</div>
 	
