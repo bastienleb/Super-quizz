5124a3d6af0d153bef7ed946945d4611e8b40caf
leblet <leblet@localhost>
2020-05-17 17:05:20 +0200

    creation_salle

--- creation_salle.php
+++ creation_salle.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 <head>
 	<title>Création salle</title>
+	<link rel="stylesheet" href="creation_salle.css">
 	<meta charset="utf-8">
 </head>
 <body>
@@ -23,14 +24,21 @@
 			die("<p>connexion impossible</p>");
 		}
 	?>
+	<h1 class="titre">Création de partie</h1>
 	<form method="POST">
-		<input type="text" name="nom" placeholder="Nom de la salle">
-		<br/>
-		<input type="text" name="nombre_joueurs" placeholder="nombre de joueurs">
-		<br/>
-		<input type="text" name="nombre_questions" placeholder="nombre de questions">
+		<label>Nom de la salle:</label>
+		<input type="text" name="nom" placeholder="Nom de la salle" required>
+		<br/><br/>
+		<label>Nombre de joueurs max:</label>
+		<input type="text" name="nombre_joueurs" placeholder="nombre de joueurs" required>
+		<br/><br/>
+		<label>Nombre de question:</label>
+		<input type="text" name="nombre_questions" placeholder="nombre de questions" required>
+		<br/><br/>
+
+		<label>Choix du theme:</label>
 		<select name="nom_theme" class="selecteur">
-	    	<option value="selection" hidden="">Selectionnez</option>
+		<option value="selection" hidden="">Selectionnez</option>
 		
 			<?php
  	 			$resultat = mysqli_query($link,"SELECT nom_theme FROM Themes ");
@@ -48,7 +56,7 @@
     			}
  			?>
     	</select>
-        <button type="submit" name="créer">Créer</button>
+        <br/><br/><button type="submit" name="créer">Créer</button><br/><br/>
         <a href="main.php">Retourner à la page principale</a> 
    	</form>
 
