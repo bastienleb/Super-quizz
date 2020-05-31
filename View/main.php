a5e226f0593258c2e8b9c64de1903cdccf278bb3
quintois <arnaud.quintois@etu.u-pec.fr>
2020-05-31 18:37:35 +0000

    main.php

---
+++ View/main.php
@@ -0,0 +1,51 @@
+<?php //require('../Model/VerifSession2.php'); ?>
+<?php require('../Model/ListeThemes.php'); ?>
+<?php require('../Model/SelecteurThemes.php'); ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+        <meta charset="utf-8">
+	    <title>page principale </title>
+	    <link rel="stylesheet" href="../public/CSS/main.css">
+    </head>
+    <body>
+    <ul>
+        <li > <a class="nom" style="float:left">Connecté en tant que :<b> <?=$Username?></b></a></li>
+        <li style="float:center" class="titre"><b><h3>SUPER QUIZZ</h3></b></li>
+        <div class="dropdown">
+            <button class="dropbtn">Parametre</button>
+            <div class="dropdown-content">
+                <a href="#">Perso</a>
+                <a href="deco.php">Déconexion</a>
+            </div>
+        </div>
+    </ul>
+        <!--////////////////////////////////////////////////////////////////////affichage theme-->
+    <div class="theme">
+        <select name="theme" class="selecteur">
+        <option value="selection" hidden="" selected="selected">Theme</option>
+    </div>
+
+        <div class="gauche">
+        <h4>Partie en cours </h4>
+                    <!---------------------affichage partie-->
+        </div>
+
+        
+        <!--//////////////////////////////////////////////////////////////////////////////button-->
+        <div class="droite">
+            <h4>Parametrer le jeu </h4>
+            <form method="post" action="creation_questions.php">
+                <button name="set_question" class= "buttonq">Crée un set de question </button>
+            </form>
+
+            <form method="post" action="creation_salle.php">
+                <button name="set_question" class="buttonp">Crée une partie</button>
+            </form>
+            <h4>Theme deja existant</h4>
+        </div>
+
+        
+	</body>
+</html>
