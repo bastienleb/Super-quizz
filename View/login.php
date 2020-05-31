6aac7ab18213130c6d6c62f1a0c65495fb123373
quintois <arnaud.quintois@etu.u-pec.fr>
2020-05-31 16:21:37 +0000

    login.php

---
+++ View/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+	    <meta charset="utf-8">
+	    <title>connection </title>
+	    <link rel="stylesheet" href="../public/CSS/login.css">
+	</head>
+	<body>
+	<div class="all">
+		<img src='../public/Images/quizz_img.jpg'>
+		<form method = "post"action="../Controller/index.php">
+			<label>Username: </label><br/>
+			<input type="text" name="Username" required>
+		
+			</br>
+			</br>	
+		
+			<label>Password: </label><br/>
+			<input type="password" name="Password" required>
+
+			</br>
+			</br>
+
+			<button>se connecter</button>
+		</form>
+		<a href="https://dwarves.iut-fbleau.fr/~quintois/projet_wim/new_compte.php">Crée un compte</a>
+	</div>	
+	</body>
+</html>
