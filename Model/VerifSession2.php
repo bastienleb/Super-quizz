7f1f888f84422fb1a82a2bc455b3dd54ace0dab5
quintois <arnaud.quintois@etu.u-pec.fr>
2020-05-31 18:36:07 +0000

    VerifSession

---
+++ Model/VerifSession2.php
@@ -0,0 +1,17 @@
+<?php
+	session_start(); 
+	if(isset($_SESSION['Username'])){
+		echo ("SESSION <br>" . " Pseudo : " . $_SESSION['Username']);
+	}
+	else if(isset($_POST['Username'])){
+		$_SESSION['Username'] = $_POST['Username'];
+
+		setcookie('pseudo', $_POST['Username']);
+
+		echo("POST <br>" ." Pseudo : " . $_SESSION['Username'] . "<br>");
+	}
+	else{
+		header('Location: ../View/login.php');
+		//echo "redirect";
+	}
+?>
