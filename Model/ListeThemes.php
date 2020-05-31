6c2a24da5ca16de4f69d1d42d262d890e86e9e88
quintois <arnaud.quintois@etu.u-pec.fr>
2020-05-31 18:36:53 +0000

    main

---
+++ Model/ListeThemes.php
@@ -0,0 +1,16 @@
+<?php
+
+require('connection.php');
+
+$resultat = mysqli_query($link,"SELECT * FROM Themes ");
+    if($resultat) {
+        while($nom=mysqli_fetch_object($resultat)) {
+            echo "<option>";
+            echo $nom->nom_theme;
+            echo "</option>";
+		}
+        echo "</select>";
+    } else {
+    	die("<p>erreur dans la requete<p>");
+    }
+?>
---
+++ Model/SelecteurThemes.php
@@ -0,0 +1,32 @@
+<?php
+require('connection.php');
+
+$bpartie = mysqli_query($link, "SELECT * FROM  ListeTables");
+    if($bpartie) {
+?>
+<table>
+<tr>
+<th>nom</th>
+<th>nombre de joueurs max</th>
+<th>theme</th>
+<th>entrer</th>
+</tr>
+<?php
+    while($apartie=mysqli_fetch_object($bpartie)) {
+        echo "<tr>";
+        echo "<td>".$apartie->Nom."</td>";
+        echo "<td>".$apartie->nombre_joueurs."</td>";
+        echo "<td>".$apartie->theme."</td>";
+        echo "<td>";
+?><input type="button" value="entrer"><?php
+    echo "</td>";
+    ?>
+               
+<?php
+    echo "</tr>";
+	}
+    echo "</table>";
+    } else {
+	die("<p>erreur dans la requete<p>");
+    }
+?>	
