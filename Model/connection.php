b7788e93bb1e4604da1b780a8ac2c54a34e6db78
quintois <arnaud.quintois@etu.u-pec.fr>
2020-05-31 17:43:49 +0000

    connection

--- Model/connection.php
+++ Model/connection.php
@@ -1,8 +1,6 @@
 <?php
-	function get_connect(){
-		 $link=mysqli_connect("localhost","quintois","quintois","quintois");
+		$link=mysqli_connect("localhost","quintois","quintois","quintois");
     	if(!$link){
         	die("<p>connexion impossible</p>");
     	}
-	}
 ?>
