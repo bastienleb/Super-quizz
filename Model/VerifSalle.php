240951646031f87ccf1aca607cd69e40456690a0
quintois <arnaud.quintois@etu.u-pec.fr>
2020-06-01 16:30:44 +0000

    VerifSalle

--- Model/VerifSalle.php
+++ Model/VerifSalle.php
@@ -9,9 +9,20 @@
 		$nombre_questions = mysqli_real_escape_string($link, $_POST['nombre_questions']);
 		$nom_theme = mysqli_real_escape_string($link, $_POST['nom_theme']);
 
-  	/*$salle = mysqli_prepare($link,"INSERT INTO ListeTables (Createur,Nom,nombre_joueurs,nombre_questions,theme) VALUES (?,?,?,?,?)");
+  	$search = mysqli_query($link,"SELECT COUNT(*) AS existe_salle FROM ListeTables WHERE Nom = '".$nom."'");
+    $data = mysqli_fetch_assoc($search);
+
+    if(($data['existe_salle'] != '0')){
+      echo "Erreur: Nom de table déjà utilisé !";
+    }else{
+
+   /* $salle = mysqli_prepare($link,"INSERT INTO ListeTables (Createur,Nom,nombre_joueurs,nombre_questions,theme) VALUES (?,?,?,?,?)");
   	mysqli_stmt_bind_param($salle,"ssdds",$user,$nom,$nombre_joueurs,$nombre_questions,$nom_theme);
-  	mysqli_execute($salle); */
-  	//header("location: room.php");
+  	mysqli_execute($salle);
+
+    session_start();
+    $SESSION['nom'] = $nom;
+  	header("location: Room.php");*/
+    } 
   }
 ?>
