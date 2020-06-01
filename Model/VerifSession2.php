b831076dd91c8b9ff6622e4b7789f3ae0c851a5e
quintois <arnaud.quintois@etu.u-pec.fr>
2020-06-01 15:10:07 +0000

    Verif

--- Model/VerifSession2.php
+++ Model/VerifSession2.php
@@ -1,13 +1,13 @@
 <?php
-	if(!isset($_COOKIE['user'])) {
-       	if(isset($_POST['user'])) {
-       	    setcookie('user', $_POST['user']);
-       	    $user = json_decode($_POST['user']); //////////////// A METTRE DANS LA PAGE APRES CETTE PAGE 
+	if(!isset($_COOKIE['Username'])) {
+       	if(isset($_POST['Username'])) {
+       	    setcookie('Username', $_POST['Username']);
+       	    $Username = json_decode($_POST['Username']); //////////////// A METTRE DANS LA PAGE APRES CETTE PAGE 
        	}
        	else {
-       	    header('Location: https://dwarves.iut-fbleau.fr/~leblet/WIM/PROJET/login.php');
+       	    header('Location: ../View/login.php');
        	}
    	} else {
-       	$user = json_decode($_COOKIE['user']);
+       	$Username = json_decode($_COOKIE['Username']);
    	}
 ?>
