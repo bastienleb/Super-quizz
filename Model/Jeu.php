fa40803ad17cc818649e2aa7b057e2c8b5bbf074
quintois <arnaud.quintois@etu.u-pec.fr>
2020-06-02 15:30:54 +0000

    Jeu

--- Model/Jeu.php
+++ Model/Jeu.php
@@ -1,8 +1,18 @@
 <?php
 	
 	require('connection.php');
+	if(isset($_GET['nom'])){
+		$search = mysqli_query($link,"SELECT COUNT(*) AS existe_salle FROM ListeTables WHERE Nom = '".$_GET['nom']."'");
+    	$data = mysqli_fetch_assoc($search);
 
-	echo $_GET['nom'];
+    	if(($data['existe_salle'] == '0')){
+    		header('Location:../View/main.php');
+    	} else
+			echo $_GET['nom'];
+	} else{
+		echo "La salle n'existe pas, redirection";
+		header('Location: ../View/main.php');
+	}
 	/**session_start();
 	if(isset($_SESSION['nom'])){
 		$nom_salle = $_SESSION['nom'];
