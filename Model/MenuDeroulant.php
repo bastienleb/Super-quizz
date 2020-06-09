154b673b4541441a1c8b586b676c6c378bf3b5ae
quintois <arnaud.quintois@etu.u-pec.fr>
2020-06-09 23:09:57 +0000

    main

--- Model/MenuDeroulant.php
+++ Model/MenuDeroulant.php
@@ -6,15 +6,15 @@
             $resultat = mysqli_query($link,"SELECT * FROM Themes ");
             if($resultat) {
                 while($nom=mysqli_fetch_object($resultat)) {
-                    echo "<script type='text/javascript'>
+                    /*echo "<script type='text/javascript'>
                         var newOption = document.createElement('option');
                         newOption.value = '<?php echo $nom->nom_theme; ?>'
                         newOption.innerHTML = '<?php echo $nom->nom_theme; ?>'
                         document.getElementById('selecte').appendChild(newOption);
-                    </script>";
-                    /*echo "<option>";
+                    </script>"; */
+                    echo "<option>";
                     echo $nom->nom_theme;
-                    echo "</option>";*/
+                    echo "</option>";
                 }
                 echo "</select>";
             } else {
