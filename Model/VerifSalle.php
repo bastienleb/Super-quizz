d8a4ad71b9101927724df6a33dbd669d312305e2
quintois <arnaud.quintois@etu.u-pec.fr>
2020-06-09 22:54:22 +0000

    creation_salle

--- Model/VerifSalle.php
+++ Model/VerifSalle.php
@@ -1,28 +1,35 @@
 <?php
-    
-  require('connection.php');
+    class VerifSalle {
+        public function VerificSalle() {
 
-	if(isset($_POST['nom']) && isset($_POST['nombre_joueurs']) && isset($_POST['nombre_questions']) && isset($_POST['nom_theme'])){
-		$user = 'test';	
-		$nom = mysqli_real_escape_string($link, $_POST['nom']);
-		$nombre_joueurs = mysqli_real_escape_string($link, $_POST['nombre_joueurs']);
-		$nombre_questions = mysqli_real_escape_string($link, $_POST['nombre_questions']);
-		$nom_theme = mysqli_real_escape_string($link, $_POST['nom_theme']);
+            $link = connection::connect();
 
-  	$search = mysqli_query($link,"SELECT COUNT(*) AS existe_salle FROM ListeTables WHERE Nom = '".$nom."'");
-    $data = mysqli_fetch_assoc($search);
+            if(isset($_POST['nom']) && isset($_POST['nombre_joueurs']) && isset($_POST['nombre_questions']) && isset($_POST['nom_theme'])){
+                $user = 'test'; 
+                $nom = mysqli_real_escape_string($link, $_POST['nom']);
+                $nombre_joueurs = mysqli_real_escape_string($link, $_POST['nombre_joueurs']);
+                $nombre_questions = mysqli_real_escape_string($link, $_POST['nombre_questions']);
+                $nom_theme = mysqli_real_escape_string($link, $_POST['nom_theme']);
 
-    if(($data['existe_salle'] != '0')){
-      echo "Erreur: Nom de table déjà utilisé !";
-    }else{
+                $search = mysqli_query($link,"SELECT COUNT(*) AS existe_salle FROM ListeTables WHERE Nom = '".$nom."'");
+                $data = mysqli_fetch_assoc($search);
 
-    $salle = mysqli_prepare($link,"INSERT INTO ListeTables (Createur,Nom,nombre_joueurs,nombre_questions,theme) VALUES (?,?,?,?,?)");
-  	mysqli_stmt_bind_param($salle,"ssdds",$user,$nom,$nombre_joueurs,$nombre_questions,$nom_theme);
-  	mysqli_execute($salle);
+                if(($data['existe_salle'] != '0')){
+                    echo "Erreur: Nom de table déjà utilisé !";
+                }else{
 
-    /*session_start();
-    $_SESSION['nom'] = $nom; */
-  	header("location: Room.php?nom=".$nom."");
-    } 
-  }
+                    $NomSalle = explode("<", $nom);
+                    //var_dump ($NomSalle);
+                    var_dump($nom_theme);
+                    /*$salle = mysqli_prepare($link,"INSERT INTO ListeTables (Createur,Nom,nombre_joueurs,nombre_questions,theme) VALUES (?,?,?,?,?)");
+                    mysqli_stmt_bind_param($salle,"ssdds",$user,$NomSalle,$nombre_joueurs,$nombre_questions,$nom_theme);
+                    mysqli_execute($salle); */
+
+                    /*session_start();
+                    $_SESSION['nom'] = $nom; */
+                    //header("location: Room/$nom");
+                }
+            }
+        } 
+    }
 ?>
--- View/creation_salle.php
+++ View/creation_salle.php
@@ -1,11 +1,9 @@
 <?php //require('../Model/VerifSession2.php'); ?>
-<?php require('../Model/VerifSalle.php'); ?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
 	<title>Création salle</title>
-	<link rel="stylesheet" href="../public/CSS/creation_salle.css">
+	<link rel="stylesheet" href="https://dwarves.iut-fbleau.fr/~quintois/projet_wim/public/CSS/creation_salle.css">
 	<meta charset="utf-8">
 </head>
 <body>
@@ -23,11 +21,10 @@
 
 		<label>Choix du theme:</label>
 		<select name="nom_theme" class="selecteur">
-		<option value="selection" hidden="">Selectionnez</option>
-			<?php require('../Model/ListeThemes.php'); ?>
-    	</select>
+			<option value="selection" hidden="">Selectionnez</option>
+        </select>
         <br/><br/><button type="submit" name="créer">Créer</button><br/><br/>
-        <a href="main.php">Retourner à la page principale</a> 
+        <a href="main">Retourner à la page principale</a> 
    	</form>
 </body>
 </html>
