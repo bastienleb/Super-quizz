01e648c6d23a9a52d587b1aa70b6208b9d09e107
quintois <arnaud.quintois@etu.u-pec.fr>
2020-06-10 09:16:47 +0000

    creation_salle

--- View/creation_salle.php
+++ View/creation_salle.php
@@ -20,8 +20,11 @@
 		<br/><br/>
 
 		<label>Choix du theme:</label>
-		<select name="nom_theme" class="selecteur">
+		<select name="nom_theme" class="selecteur" id='selecte'>
 			<option value="selection" hidden="">Selectionnez</option>
+			<option value="Math">Math</option>
+			<option value="Francais">Francais</option>
+			<option value="sneakers">Sneakers</option>
         </select>
         <br/><br/><button type="submit" name="créer">Créer</button><br/><br/>
         <a href="main">Retourner à la page principale</a> 
