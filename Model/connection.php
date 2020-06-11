619153a7db0c77c23892f0c27cad092ce43359d5
quintois <arnaud.quintois@etu.u-pec.fr>
2020-06-11 17:18:07 +0000

    bdd

--- Model/connection.php
+++ Model/connection.php
@@ -2,7 +2,6 @@
 	class connection {
 		public static function Connect() {
 			$link = mysqli_connect("localhost","quintois","quintois","quintois");
-			//var_dump($link);
 			if(!$link)
 				die("<p>connexion impossible</p>");
 			return $link;
