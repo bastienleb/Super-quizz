6228dd3a927cb331705ec47b70927a6f485fc4bb
leblet <leblet@localhost>
2020-06-11 11:53:45 +0200

    Jeu

--- Model/Question.php
+++ Model/Question.php
@@ -8,15 +8,16 @@
                 while($nom=mysqli_fetch_object($resultat)) {
                     /*echo "<b>".$nom->Theme;*/
             
-            
-                    echo "<div class='question'><b>".$nom->Question." ? </b></div></br>";
-            
-                    echo "<div id='reponses' class='reponses'>";
+                    echo "<div class='question_class'>";
+                        echo "<div class='question'><b>".$nom->Question." ? </b></div></br>";
+                
+                        echo "<div id='reponses' class='reponses'>";
 
-                    echo "<div id='reponse1' class='reponse_1'>".$nom->Reponse_A."</div>";
-                    echo "<div id='reponse1' class='reponse_2'>".$nom->Reponse_B."</div></br>";
-                    echo "<div id='reponse1' class='reponse_3'>".$nom->Reponse_C."</div>";
-                    echo "<div id='reponse1' class='reponse_4'>".$nom->Reponse_D."</div>";
+                        echo "<div id='reponse1' class='reponse_1'>".$nom->Reponse_A."</div>";
+                        echo "<div id='reponse1' class='reponse_2'>".$nom->Reponse_B."</div></br>";
+                        echo "<div id='reponse1' class='reponse_3'>".$nom->Reponse_C."</div>";
+                        echo "<div id='reponse1' class='reponse_4'>".$nom->Reponse_D."</div>";
+                    echo "</div>";
                 }
             }else {
                 die("<p>erreur dans la requete<p>");
