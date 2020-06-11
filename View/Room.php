9f605008cff6744196ba86b4b0e6f443fecbdc0f
leblet <leblet@localhost>
2020-06-11 22:50:40 +0200

    Jeu

--- View/Room.php
+++ View/Room.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" type="text/css" href="https://dwarves.iut-fbleau.fr/~quintois/projet_wim/public/CSS/Room.css">
 </head>
 <body>
-    <div class="all">
+    <div class="all" id="test">
         
         <?php //require('../Model/Question.php'); ?>
 
@@ -14,7 +14,32 @@
         </div>        
         <br>
 
-        <button onclick="move()">Commencer</button>
+        <button onclick="test()" id="button">Commencer</button>
     </div>
 </body>
-</html
+
+<script>
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 0;
+    var id = setInterval(frame, 50 /*valeur a changer pour le temps*/);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+        move();
+
+        /*location.replace("main.php");*/
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width ;
+      }
+    }
+  }
+}
+</script>
+</html>
