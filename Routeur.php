6f437b6571d3dec02234e34a5760987d1d457d34
quintois <arnaud.quintois@etu.u-pec.fr>
2020-06-11 23:55:34 +0000

    Quizz_Panic

--- Routeur.php
+++ Routeur.php
@@ -28,7 +28,7 @@
 				return $result;
 			}
 
-			if((count($parts) == 3) && ($parts[0] == "Room")){
+			if((count($parts) == 4) && ($parts[0] == "Room")){
 				$result['controller'] = 'RoomController';
 				$result['action'] = $parts[1];
 				return $result;
