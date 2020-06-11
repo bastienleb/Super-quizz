d22c96a01e9697d0829263a06637ca144fd40530
quintois <arnaud.quintois@etu.u-pec.fr>
2020-06-11 00:22:28 +0000

    verifSession

--- Model/VerifSession1.php
+++ Model/VerifSession1.php
@@ -1,6 +1,6 @@
 <?php
-		session_start();
-		if(isset($_SESSION['Username']) && isset($_SESSION['Salon'])){
-			header('Location: ../View/main.php');
-		}
-	?>
+	session_start();
+	if(isset($_SESSION['Username'])){
+		header('Location: ../View/main.php');
+	}
+?>
