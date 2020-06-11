ed8cebdd6a665aad95fcea84e7fb0b0a9f58a1e1
leblet <leblet@localhost>
2020-06-11 22:51:25 +0200

    Jeu

--- Model/Question.php
+++ Model/Question.php
@@ -3,24 +3,31 @@
         public function AfficherQuestions() {
             $link = connection::Connect();
 
-            $resultat = mysqli_query($link,"SELECT * FROM Questions WHERE Theme= 'sneacker' "); 
-            if($resultat) {
-                while($nom=mysqli_fetch_object($resultat)) {
-                    /*echo "<b>".$nom->Theme;*/
-            
-                    echo "<div class='question_class'>";
-                        echo "<div class='question'><b>".$nom->Question." ? </b></div></br>";
-                
-                        echo "<div id='reponses' class='reponses'>";
+            $query = isset($_GET['query']) ? $_GET['query'] : "";
+			$parts = explode("/", $query);			
 
-                        echo "<div id='reponse1' class='reponse_1'>".$nom->Reponse_A."</div>";
-                        echo "<div id='reponse1' class='reponse_2'>".$nom->Reponse_B."</div></br>";
-                        echo "<div id='reponse1' class='reponse_3'>".$nom->Reponse_C."</div>";
-                        echo "<div id='reponse1' class='reponse_4'>".$nom->Reponse_D."</div>";
-                    echo "</div>";
+			if(isset($parts[2])){
+                $theme = $parts[2];
+                
+                $resultat = mysqli_query($link,"SELECT * FROM Questions WHERE Theme= '$theme' "); 
+                if($resultat) {
+                    while($nom=mysqli_fetch_object($resultat)) {
+                        /*echo "<b>".$nom->Theme;*/
+                
+                        echo "<div class='question_class'>";
+                            echo "<div class='question'><b>".$nom->Question."  </b></div></br>";
+                    
+                            echo "<div id='reponses' class='reponses'>";
+
+                            echo "<div id='reponse1' class='reponse_1'>".$nom->Reponse_A."</div>";
+                            echo "<div id='reponse1' class='reponse_2'>".$nom->Reponse_B."</div></br>";
+                            echo "<div id='reponse1' class='reponse_3'>".$nom->Reponse_C."</div>";
+                            echo "<div id='reponse1' class='reponse_4'>".$nom->Reponse_D."</div>";
+                        echo "</div>";
+                    }
+                }else {
+                    die("<p>erreur dans la requete<p>");
                 }
-            }else {
-                die("<p>erreur dans la requete<p>");
             }
         }
     }
