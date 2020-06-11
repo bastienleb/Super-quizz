ba4e405c0634c96a44462c5298ff71a8799cc2b3
quintois <arnaud.quintois@etu.u-pec.fr>
2020-06-11 08:54:32 +0000

    Jeu

--- Controller/RoomController.php
+++ Controller/RoomController.php
@@ -3,6 +3,7 @@
 		public function ControllerJeu() {
 			echo file_get_contents(__DIR__.'/../View/Room.php');
 			Jeu::Jouer();
+			Question::AfficherQuestions();
 		}
 	}
 ?>
--- Model/Question.php
+++ Model/Question.php
@@ -1,24 +1,26 @@
 <?php
+    class Question {
+        public function AfficherQuestions() {
+            $link = connection::Connect();
 
-require('connection.php');
-
-
-$resultat = mysqli_query($link,"SELECT * FROM Questions WHERE Theme= 'sneacker' "); 
-    if($resultat) {
-        while($nom=mysqli_fetch_object($resultat)) {
-            /*echo "<b>".$nom->Theme;*/
+            $resultat = mysqli_query($link,"SELECT * FROM Questions WHERE Theme= 'sneacker' "); 
+            if($resultat) {
+                while($nom=mysqli_fetch_object($resultat)) {
+                    /*echo "<b>".$nom->Theme;*/
             
             
-            echo "<div class='question'><b>".$nom->Question." ? </b></div></br>";
+                    echo "<div class='question'><b>".$nom->Question." ? </b></div></br>";
             
-            echo "<div id='reponses' class='reponses'>";
+                    echo "<div id='reponses' class='reponses'>";
 
-            echo "<div id='reponse1' class='reponse_1'>".$nom->Reponse_A;."</div>";
-            echo "<div id='reponse1' class='reponse_2'>".$nom->Reponse_B;."</div></br>";
-            echo "<div id='reponse1' class='reponse_3'>".$nom->Reponse_C;."</div>";
-            echo "<div id='reponse1' class='reponse_4'>".$nom->Reponse_D;."</div>";
+                    echo "<div id='reponse1' class='reponse_1'>".$nom->Reponse_A."</div>";
+                    echo "<div id='reponse1' class='reponse_2'>".$nom->Reponse_B."</div></br>";
+                    echo "<div id='reponse1' class='reponse_3'>".$nom->Reponse_C."</div>";
+                    echo "<div id='reponse1' class='reponse_4'>".$nom->Reponse_D."</div>";
+                }
+            }else {
+                die("<p>erreur dans la requete<p>");
+            }
         }
-    }else {
-        die("<p>erreur dans la requete<p>");
     }
 ?>
