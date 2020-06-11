3e2c474530c2a2160656c2544b85a71544f090cf
quintois <arnaud.quintois@etu.u-pec.fr>
2020-06-11 00:23:34 +0000

    ModifCompte

---
+++ View/perso.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title>Personnalisation</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="">
+</head>
+<body>
+	<div class="all">
+		<h1>Que souhaitez vous faire ?</h1>
+		</br></br>
+		<form method="post">
+			<p>Modifier son pseudo:</p>
+			<input type="text" name="ModifierPseudo" placeholder="Nouveau Pseudo"></br></br>
+			<input type="submit" name="Changer" value="Changer">
+		</form>
+		</br></br>
+		<form method="post">
+			<p>Modifier son mot de passe:</p>
+			<input type="password" name="AncienMDP" placeholder="Ancien mot de passe"></br></br>
+			<input type="password" name="NouveauMDP" placeholder="Nouveau mot de passe"></br></br>
+			<input type="password" name="ConfirmMDP" placeholder="Confirmer le mot de passe"></br></br>
+			<input type="submit" name="Changer" value="Changer">
+		</form>
+		<a href="main">Retourner à la page principale</a> 
+	</div>
+</body>
+</html>
