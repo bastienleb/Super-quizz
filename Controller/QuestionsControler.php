0caf77b767492b818a7241a1faa40c3243614fd3
quintois <quintois@localhost>
2020-06-12 22:41:23 +0200

    Supprimer 'Controller/QuestionsControler.php'

--- Controller/QuestionsControler.php
+++
@@ -1,9 +0,0 @@
-<?php
-   	class QuestionsController {
-/* ------------------Redirection vers creation question------------------ */
-   		public function VerifierQuestions() {
-   			require(__DIR__.'/../View/creation_questions.php');
-   			VerifQuestions::VerifQuestion();
-		}
-  	}
-?>
