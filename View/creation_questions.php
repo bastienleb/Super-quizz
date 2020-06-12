f4fa21d3ef34da2e08b3f931a6ba679f16ff1109
quintois <arnaud.quintois@etu.u-pec.fr>
2020-06-12 20:40:41 +0000

    creation_questions

--- View/creation_questions.php
+++ View/creation_questions.php
@@ -13,6 +13,10 @@
             <option value="Math">Math</option>
             <option value="Francais">Francais</option>
             <option value="sneakers">Sneakers</option>
+            <option value="Musique">Musique</option>
+            <option value="Sport">Sport</option>
+            <option value="Cinema">Cinema</option>
+            <option value="Series">Series</option>
         </select>
     	<br/><br/>
 
