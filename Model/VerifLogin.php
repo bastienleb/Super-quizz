7b7d2cd8f08f5c583c9505220841dd440a5a966f
quintois <arnaud.quintois@etu.u-pec.fr>
2020-06-12 20:11:24 +0000

    VerifLogin.php

---
+++ Model/VerifLogin.php
@@ -0,0 +1,34 @@
+<?php
+   	class VerifLogin {
+   		public function Verif() {
+   			$link = connection::Connect();
+   			session_start();
+   			if(isset($_SESSION['Username']))
+   				header('Location: main');
+				if(isset($_POST['Username']) && isset($_POST['Password'])) {
+   					$Pseudo = mysqli_real_escape_string($link, $_POST['Username']);
+       				$MDP = mysqli_real_escape_string($link, $_POST['Password']);
+       			} else {
+       			$Pseudo = "///";
+       			$MDP = "$$$";
+   			}
+
+/*--------------------On compare le mdp haché avec le mdp de la bdd----*/
+
+   			$test = mysqli_query($link,"SELECT mdp FROM login WHERE pseudo = '".$Pseudo."'");
+   			$password = "Bon" . $Pseudo . "jour" . $MDP. "aussi";
+   			$password = hash('sha512', $password);
+			$hache = mysqli_fetch_array($test);
+			
+			if($Pseudo!="///" && $MDP!="$$$"){
+				if($hache['mdp'] == $password ){
+					$_SESSION['Username'] = $Pseudo;
+					echo "<div class='bon'> Connection réussie <br></div>";
+					header('Location: main');
+				} else {
+					echo "<div class='error'> Mauvais mot de passe <br></div>";
+				}
+			}
+   		}
+   	}
+?>
