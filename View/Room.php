3598490229b7d3c4470bdc3471d560c405e060b6
leblet <leblet@localhost>
2020-06-13 13:56:30 +0200

    Jeu

--- View/Room.php
+++ View/Room.php
@@ -4,13 +4,16 @@
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="https://dwarves.iut-fbleau.fr/~quintois/projet_wim/public/CSS/Room.css">
 </head>
-<body>
-    <div class="all" id="test">        
+<body>      
         <form>
             <div class='question_class'>
+            <h4> Question :</h4>
                 <div class='question'><b><?php echo json_encode($questions['question']); ?></b>
                 </div><br/>
             </div>
+
+            <div class="all">  
+            <h4> Reponses :</h4>
             <div id='reponses' class='reponses'>
                 <div id='reponse1' class='reponse_1'><?php echo json_encode($questions['reponseA']); ?></div>
                 <div id='reponse1' class='reponse_2'><?php echo json_encode($questions['reponseB']); ?></div></br>
@@ -23,6 +26,7 @@
             <br>
             <button onclick="move(); return false;" id="button">Commencer</button>
         </form>
+        </br></br>
         <a href="../../../main">Retourner au main</a>
     </div>
 </body>
