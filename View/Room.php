e38564b70865a9eca93bea0f9cce9d839ee20ae0
quintois <arnaud.quintois@etu.u-pec.fr>
2020-06-13 12:15:42 +0000

    Jeu

--- View/Room.php
+++ View/Room.php
@@ -8,17 +8,17 @@
         <form>
             <div class='question_class'>
             <h4> Question :</h4>
-                <div class='question'><b><?php echo json_encode($questions['question']); ?></b>
+                <div class='question'><b><?php echo ($questions['question']); ?></b>
                 </div><br/>
             </div>
 
             <div class="all">  
             <h4> Reponses :</h4>
             <div id='reponses' class='reponses'>
-                <div id='reponse1' class='reponse_1'><?php echo json_encode($questions['reponseA']); ?></div>
-                <div id='reponse1' class='reponse_2'><?php echo json_encode($questions['reponseB']); ?></div></br>
-                <div id='reponse1' class='reponse_3'><?php echo json_encode($questions['reponseC']); ?></div>
-                <div id='reponse1' class='reponse_4'><?php echo json_encode($questions['reponseD']); ?></div>;
+                <div id='reponse1' class='reponse_1'><?php echo ($questions['reponseA']); ?></div>
+                <div id='reponse1' class='reponse_2'><?php echo ($questions['reponseB']); ?></div></br>
+                <div id='reponse1' class='reponse_3'><?php echo ($questions['reponseC']); ?></div>
+                <div id='reponse1' class='reponse_4'><?php echo ($questions['reponseD']); ?></div>;
             </div>
             <div id="myProgress">
                 <div id="myBar">0</div> 
