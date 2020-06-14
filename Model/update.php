8f63078e5448b14c643d825a5a881555761d284b
leblet <leblet@localhost>
2020-06-15 01:26:24 +0200

    Jeu

--- Model/update.php
+++ Model/update.php
@@ -1,16 +1,17 @@
 <?php
     session_start();
+   // echo "<p>test</p>";
+    $link = mysqli_connect("localhost","quintois","quintois","quintois");
+        
+    if(isset($_POST['score'])){
+    //echo "test dans if ";
+	$score =  mysqli_real_escape_string($link, $_POST['score']);
+    $nom = $_SESSION['Username'];
+            
+
+	$update = "UPDATE Nombre_online SET Score = '".$score."' WHERE Pseudo = '".$nom."'";
     
-
-    $link = connection::Connect();
-
-    if(isset($_POST['score_fin'])){
-    	$score =  mysqli_real_escape_string($link, $_POST['score_fin']);
-    	$nom = $_SESSION['Username'];
-
-    	$update = "UPDATE Nombre_online SET Score = '".$score."' WHERE Pseudo = '".$nom."'";
-
-    	$maj = mysqli_query($link, $update);
-    }
-
+    $maj = mysqli_query($link, $update);
+    echo $score;
+}
 ?>
