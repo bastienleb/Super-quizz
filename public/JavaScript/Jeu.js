a75c25114894b43354ff18960733cd4a3ea672ba
leblet <leblet@localhost>
2020-06-15 01:26:51 +0200

    js

--- public/JavaScript/Jeu.js
+++ public/JavaScript/Jeu.js
@@ -1,10 +1,7 @@
 var i = 0;
 var id = 0;
 var conv= 0;
-var score=0;
 
-
-var xhr = new XMLHttpRequest();
 var reponses = document.getElementById('test');
 var question = document.getElementById('question');
 var repnse1 = document.getElementById('reponse1');
@@ -12,6 +9,7 @@ var repnse2 = document.getElementById('reponse2');
 var repnse3 = document.getElementById('reponse3');
 var repnse4 = document.getElementById('reponse4');
 var bonrepnse = document.getElementById('bonnereponse');
+var score = document.getElementById('demo')
 
 var ContenuQuestion = question.textContent;
 var ContenuReponse1 = reponse1.textContent;
@@ -19,6 +17,8 @@ var ContenuReponse2 = reponse2.textContent;
 var ContenuReponse3 = reponse3.textContent;
 var ContenuReponse4 = reponse4.textContent;
 var ContenuBonreponse = bonrepnse.textContent;
+var ContenuScore = score.textContent;
+
 /*------------------↑ Recup valeur et initialisation ↑------------------*/
 
 document.getElementById('reponses').style.display = 'none';
@@ -31,7 +31,6 @@ function move() {
   document.getElementById('question').style.display = 'inline-block';
   document.getElementById('button').style.display = 'none';
   ChangeColor();
-  Affichage_score();
 
   if (i == 0) {
     i = 1;
@@ -44,27 +43,16 @@ function move() {
     console.log("reponse3 = "+ContenuReponse3);
     console.log("reponse4 = "+ContenuReponse4);
     console.log("la bonne reponse est la : "+ContenuBonreponse);
-    console.log("\nscore = "+score)
+    console.log("\nScore =  "+ContenuScore)
     
 
-    xhr.addEventListener('readystatechange', function() {
-      if(this.readyState == 4 && this.status == 200) {
-       // reponses.innerHTML = this.responseText;
-      }
-    });
-
-    xhr.open('GET', 'Question.php?question='+question, true);
-    xhr.send();
-
-    var time = setInterval(frame, 50 /*valeur a changer pour le temps*/);
+    var time = setInterval(frame, 100 /*valeur a changer pour le temps*/);
     function frame() {
       if (width >= 100) {
         clearInterval(time);
         i = 0;
-        Affichage_score();
         verfication();
-        reload_tmp();
-        //reload();
+        reload_solo();
       } else {
         width++;
         elem.style.width = width + "%";
@@ -102,23 +90,26 @@ function verfication(getId){
 
   if (conv == ContenuBonreponse) {
     console.log("\n\nVictoire");
-    score = score+1;
-    console.log("\nscore = "+ score);
+    const i = Number("1");
+    const conv_score = Number(ContenuScore)
+    const score_fin = (conv_score + i);
+    console.log("\nscore =  "+ score_fin);
     alert("Victoire");
+    update(score_fin)
   }
   else if (id== '0'){
     console.log("\n\nLe joueur n'a pas repondu");
-    console.log("\nscore = "+ score);
+    console.log("\n"+ ContenuScore);
     alert("Il faut repondre");
   }
 
   else{
-    console.log("\n\Perdu");
-    console.log("\nscore = "+ score);
+    console.log("\nPerdu");
+    console.log("\n"+ ContenuScore);
     alert("Perdu");
   }
 }
-/*------------------- function pour changer la couleur au clique  -----------------*/
+/*------------------- function dpour changer la couleur au clique -----------------*/
 function ChangeColor(getId){
   if(id == "reponse1" ){
     document.getElementById('reponse1').style.backgroundColor = 'chartreuse';
@@ -149,28 +140,32 @@ function ChangeColor(getId){
   }
 }
 
-/*------------------- function recharger page -----------------*/
 function reload_tmp(){
   //window.location.replace('3'); 
-  rest = setInterval(location.reload, 1000);
+  location.reload();
 }
 
-/*------------------- function Affichage score -----------------*/
-function Affichage_score(){
-  var x = document.getElementById("carre").innerHTML;
-  document.getElementById("demo").innerHTML = x+score;
-  console.log("\nAffichage du score");
-  console.log("\nscore = "+ score);
+function reload_solo(){
+  reload = setInterval(reload_tmp, 3000);
 }
 
-/*
-function reload(){
-  document.getElementById('question').style.display = 'none';
-        document.getElementById('reponses').style.display = 'none';
-        console.log("ID avant le RAZ : "+id);
-        id=0;
-        console.log("\nID apres le RAZ : "+id);
-        $("body").load("../test.php");
-        document.getElementById('button').style.display = 'inline-block';
-}
-*/
+function update(score_fin){
+	var score = document.getElementById('demo').value;
+	document.getElementById('demo').value = "";
+  t = document.getElementById("demo");
+	var xhr = new XMLHttpRequest();
+				
+	xhr.addEventListener('readystatechange', function() {
+		if (this.readyState == 4 && this.status == 200) {
+			t.innerHTML = this.responseText;
+		}
+  });
+  
+  xhr.open('POST', 'http://dwarves.iut-fbleau.fr/~leblet/WIM/PROJET/Model/update.php', true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhr.send("score="+score_fin);
+};
+
+
+
+
