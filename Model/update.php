73dcce2d0c467a24e7c80cd1adb50073aa845c99
quintois <arnaud.quintois@etu.u-pec.fr>
2020-06-14 21:07:11 +0000

    m

--- Model/update.php
+++ Model/update.php
@@ -1,6 +1,16 @@
 <?php
     session_start();
+    
 
     $link = connection::Connect();
+
+    if(isset($_POST['score_fin'])){
+    	$score =  mysqli_real_escape_string($link, $_POST['score_fin']);
+    	$nom = $_SESSION['Username'];
+
+    	$update = "UPDATE Nombre_online SET Score = '".$score."' WHERE Pseudo = '".$nom."'";
+
+    	$maj = mysqli_query($link, $update);
+    }
 
 ?>
